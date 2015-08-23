633863fdfc47214b2808f7d943aaa125441af13f
Jackie Zhang <viperking@163.com>
2015-08-23 20:10:48 +0800

    finished main menu scene

---
+++ public/game/AirBaron/src/config/GameConfig.js
@@ -0,0 +1,20 @@
+/**
+ * Created by jackiezhang on 15/8/23.
+ */
+/**
+ * Created by lingjianfeng on 14-8-31.
+ */
+
+var GC = GC || {};
+
+GC.winSize = cc.size(320, 480);
+
+GC.h = GC.winSize.height;
+
+GC.w = GC.winSize.width;
+
+GC.w_2 = GC.winSize.width / 2 ;
+
+GC.h_2 = GC.winSize.height / 2;
+
+
