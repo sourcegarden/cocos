071c2fabbd286e7e5fbf1a73d85e8ade44eff87d
seraphium <viperking@163.com>
2016-01-22 15:01:38 +0800

    added new game for testing

---
+++ public/game/GameTest/src/resource.js
@@ -0,0 +1,12 @@
+var res = {
+    HelloWorld_png : "res/HelloWorld.png",
+    MainScene_json : "res/MainScene.json",
+    start_n_png : "res/start_n.png",
+    start_s_png : "res/start_s.png"
+
+};
+
+var g_resources = [];
+for (var i in res) {
+    g_resources.push(res[i]);
+}
